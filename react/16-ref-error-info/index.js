0c30b441913687f99f3be17317446cbd6c532bfe
zangguojun <1739932260@qq.com>
2021-06-24 14:47:50 +0800

    22/06/24

---
+++ react/16-ref-error-info/index.js
@@ -0,0 +1,29 @@
+import React from './react'
+import ReactDOM from './react-dom'
+
+function TextInput(props) {
+  return <input ref={props.ref} />
+}
+
+class Form extends React.Component {
+  constructor() {
+    super();
+    this.formInput = React.createRef();
+  }
+
+  getFocus = () => {
+    this.formInput.current.focus();
+  }
+
+  render() {
+    return (
+      <div>
+        <TextInput ref={this.formInput} />
+        <button onClick={this.getFocus}>获取焦点</button>
+      </div>
+    )
+  }
+
+}
+
+ReactDOM.render(<Form />, document.getElementById('root'))
